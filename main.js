e2e2f24efb0d4d0ad0c2714d22aa36ae6afdf5fb
Feliks Czart <czart78@gmail.com>
2020-07-28 20:58:02 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//alert('Siema!');
+
+const firstName = 'Feliks';
+const age = 21;
+
+console.log('Siema!'); //Służy do opisywania stron lub błędów na niej w panelu programistycznym nie widocznym bezpośrednio na stronie
+console.log(firstName);
+console.log(age);
+console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
+//
