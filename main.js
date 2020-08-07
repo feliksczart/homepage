d06c555c10c2cfb854d0b16f65af6c8c5975f121
Feliks Czart <czart78@gmail.com>
2020-08-07 20:54:45 +0200

    BEM & DOM

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ console.log(firstName);
 console.log(age);
 console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
 //
+
+const heading = document.querySelector('.main-heading--js');
+
+console.log(heading);
+
+heading.innerHTML = `Nazywam się ${firstName} i mam ${age} lat.`;
